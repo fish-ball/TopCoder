18fb4c23c9e84b96eadd064ffb1cd2d1868fa11c
Alfred Huang <alfred.h@163.com>
2013-03-28 14:11:37 +0800

    Create marathon 79

---
+++ MARATHON_79/SnowCleaning.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class SnowCleaning {
+public:
+    int init(int boardSize, int salary, int snowFine) {
+    }
+    vector <string> nextDay(vector <int> snowFalls) {
+    }
+};
+
+int main() {
+    int n, s, c;
+    cin >> n >> s >> c;
+    for(int i = 2000; i--;) {
+        int cnt;
+        cin >> cnt;
+        int sf[5000];
+        for(int j = 0; j < cnt+cnt; j++) {
+            cin >> sf[j];
+        }
+        cout << 0 << endl;
+    }
+    
+    return 0;
+}
---
+++ MARATHON_79/debug.cmd
@@ -0,0 +1,1 @@
+java -jar SnowCleaningVis.jar -exec SnowCleaning -seed 3
